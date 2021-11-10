a767ef8d52b0952ebe2b19d82473953b0d8ced8a
Onion99 <lijundong@xmiles.com>
2021-11-10 14:43:49 +0800

    学习谷歌Owl设计

--- app/src/main/java/com/onion/android/app/material/owl/ui/OwlMainActivity.kt
+++ app/src/main/java/com/onion/android/app/material/owl/ui/OwlMainActivity.kt
@@ -1,13 +1,29 @@
 package com.onion.android.app.material.owl.ui
 
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import com.onion.android.R
 import com.onion.android.app.base.BindingActivity
+import com.onion.android.app.view.hide
 import com.onion.android.databinding.ActivityOwlMainBinding
 
 class OwlMainActivity : BindingActivity<ActivityOwlMainBinding>(R.layout.activity_owl_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.apply {
+            val navController = Navigation.findNavController(this@OwlMainActivity, R.id.nav_host)
+            bottomNav.setupWithNavController(navController)
+
+            lifecycleScope.launchWhenResumed {
+                navController.addOnDestinationChangedListener { _, destination, _ ->
+                    when (destination.id) {
+                        else -> bottomNav.hide()
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/onion/android/app/view/ViewExtension.kt
+++ app/src/main/java/com/onion/android/app/view/ViewExtension.kt
@@ -4,10 +4,16 @@
 package com.onion.android.app.view
 
 import android.content.res.Resources
+import android.animation.ValueAnimator
+import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.core.animation.doOnEnd
+import androidx.core.view.drawToBitmap
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.onion.android.R
 
 ///////////////////////////////////////////////////////////////////////////
@@ -45,7 +51,7 @@ val Float.px
 val Int.dp
     get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_DIP,
-        this,
+        this.toFloat(),
         Resources.getSystem().displayMetrics
     )
 
@@ -96,3 +102,33 @@ internal fun ViewGroup.updateLayoutParams(block: ViewGroup.LayoutParams.() -> Un
         layoutParams = params
     }
 }
+
+///////////////////////////////////////////////////////////////////////////
+// BottomNavigationView隐藏动画
+///////////////////////////////////////////////////////////////////////////
+fun BottomNavigationView.hide() {
+    if (visibility == View.GONE) return
+
+    val drawable = BitmapDrawable(context.resources, drawToBitmap())
+    val parent = parent as ViewGroup
+    drawable.setBounds(left, top, right, bottom)
+    parent.overlay.add(drawable)
+    visibility = View.GONE
+    ValueAnimator.ofInt(top, parent.height).apply {
+        startDelay = 100L
+        duration = 200L
+        interpolator = AnimationUtils.loadInterpolator(
+            context,
+            android.R.interpolator.fast_out_linear_in
+        )
+        addUpdateListener {
+            val newTop = it.animatedValue as Int
+            drawable.setBounds(left, newTop, right, newTop + height)
+        }
+        doOnEnd {
+            parent.overlay.remove(drawable)
+        }
+        start()
+    }
+}
+
